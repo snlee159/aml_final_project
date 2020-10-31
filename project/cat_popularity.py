14e581a50f9cbd8a7eddfc46c0612ea8cd53ffbd
Sam Lee <samanthanlee7@gmail.com>
2020-10-31 13:57:26 -0400

    more cleaning

--- project/cat_popularity.py
+++ project/cat_popularity.py
@@ -5,17 +5,19 @@ import pandas as pd
 from pytube import YouTube
 import cv2
 
-def get_video(file, nframes=10):
-    video = YouTube(file)
+def get_video(my_file):
+    video = YouTube(url=my_file)
     video.streams.filter(file_extension='mp4').all()
-    return video.streams.get_by_itag(nframes).download()
+    video.streams.get_by_itag(18).download()
+    return video
 
-def cat_popularity_classification():
-    test = get_video('https://www.youtube.com/watch?v=C9O28ne6bG8')
-    print(test)
+def cat_popularity_classification(my_file):
+    video = get_video(my_file)
+    return video
 
 def main():
-    cat_popularity_classification()
+    video = cat_popularity_classification()
+    print(video)
 
 if __name__ == "__main__":
     main()
