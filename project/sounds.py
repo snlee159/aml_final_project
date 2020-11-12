a7106d528426cb82a5ce723a10ac003f09664a96
Sam Lee <samanthanlee7@gmail.com>
2020-11-11 23:32:43 -0500

    added the sound processing library

---
+++ project/sounds.py
@@ -0,0 +1,10 @@
+# Imports
+import pandas as pd
+from aml_final_project.pyAudioAnalysis.pyAudioAnalysis import ShortTermFeatures
+from aml_final_project.pyAudioAnalysis.pyAudioAnalysis import audioBasicIO
+import matplotlib.pyplot as plt
+import os
+
+
+def extract_wav(filename):
+    os.system('ffmpeg -i {} -acodec pcm_s16le -ar 16000 {}.wav'.format(filename, actual_filename))
